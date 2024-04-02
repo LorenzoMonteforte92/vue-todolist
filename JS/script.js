39d2460b375fa444ada666cd01e9e864257e715f
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-02 13:27:49 +0200

    feat: add to do object

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            slides: [
+                {
+                    text: 'Fare la spesa',
+                    done: true,
+                }, 
+                {
+                    text: 'Andare a correre',
+                    done: true,
+                }, 
+            ]
            
         };
     },
