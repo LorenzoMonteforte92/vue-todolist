40e6d4f0806d5d5cb81c9152b003f7e0bfe95ab1
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-02 14:48:50 +0200

    feat: add create new object from user imput

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,20 @@
+// MILESTONE 1
+// Stampare all'interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": 
+//cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che viene aggiunto alla lista esistente.
+//aggiungo v-model all'imput
+//creo una variabile vuota in data che corrisponderà all'imput dellutente
+//al click parte la funzione che prende il contenuto della variabile e lo unshifta in toDo
+
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
 const { createApp } = Vue;
 
@@ -8,25 +24,45 @@ createApp({
             toDo: [
                 {
                     text: 'Fare la spesa',
-                    done: true,
+                    done: false,
                 }, 
                 {
-                    text: 'preparare schiscetta',
-                    done: true,
+                    text: 'Preparare schiscetta',
+                    done: false,
                 },
                 {
                     text: 'Andare al mare',
-                    done: true,
+                    done: false,
                 }, 
                 {
                     text: 'Pulire casa',
-                    done: true,
+                    done: false,
                 },  
-            ]
+            ],
+
+        newTask: {
+            text: '',
+            done: false,
+        },
            
         };
     },
     methods: {
+
+        removeListItem(ClickedIndex){
+            this.toDo.splice(ClickedIndex, 1)
+        },
+
+        addNewTask (){
+            this.toDo.unshift(this.newTask)
+            console.log(this.newTask)
+        },
+
+        // textStrikeThrough (ClickedIndex){
+            
+        //     // this.toDo.done = !this.toDo.done
+        //     console.log(ClickedIndex)
+        // }
        
     }
 }).mount('#app');
